609ccc258b85cd286f0fd08131ac421c5f02db39
Hashcademy <46410358+hashcademy@users.noreply.github.com>
2019-01-09 13:04:19 +0800

    Update Hashcademy.sol

--- Hashcademy.sol
+++ Hashcademy.sol
@@ -5,13 +5,34 @@ pragma solidity ^0.5.0;
 contract Hashcademy {
     string public name;
     uint public level;
+    address owner;
     
+    constructor() public {
+        owner = msg.sender;
+    }
+
     event Certificate(
         string name,
         uint level
     );
     
-    function setCertificate(string memory _name, uint _level) public {
+    modifier checkLevel(uint _level){
+        require(
+            _level <= 3,
+            "You input the wrong level."
+        );
+        _;
+    }
+    
+    modifier onlyOwner {
+        require(
+            msg.sender == owner,
+            "You must be the owner."
+        );
+        _;
+    }
+    
+    function setCertificate(string memory _name, uint _level) checkLevel(_level) onlyOwner public {
         name = _name;
         level = _level;
         emit Certificate(_name, _level);
