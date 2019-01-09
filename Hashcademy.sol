579de324be4fcc38c8be44ab1a00a4f43c073648
Hashcademy <46410358+hashcademy@users.noreply.github.com>
2019-01-09 13:00:59 +0800

    Update Hashcademy.sol

--- Hashcademy.sol
+++ Hashcademy.sol
@@ -6,9 +6,15 @@ contract Hashcademy {
     string public name;
     uint public level;
     
+    event Certificate(
+        string name,
+        uint level
+    );
+    
     function setCertificate(string memory _name, uint _level) public {
         name = _name;
         level = _level;
+        emit Certificate(_name, _level);
     }
    
     function getCertificate() public view returns (string memory , uint) {
